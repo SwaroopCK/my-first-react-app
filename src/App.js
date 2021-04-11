59b5d92eefc2562c2eb081c268674a25b391ff5b
SwaroopCK <swaroopkorde@gmail.com>
2021-04-11 13:19:42 +0530

    First-Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react'
+//Basically JS contain UI + Logic
+//You can have seperate css file
+//CSS = seperate / JS + CSS
+
+
 
 function App() {
+
+  let [counter , setCounter] = useState(0) // returns [0 , function]
+
+  function buttonWasClicked(){
+    setCounter(counter + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <button onClick = {buttonWasClicked}>Button</button> 
+      <h1>{counter}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
