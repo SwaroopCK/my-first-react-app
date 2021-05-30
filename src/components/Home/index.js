588e2cdd73e35e96e22bb44c57dfb4bbd76094d1
SwaroopCK <swaroopkorde@gmail.com>
2021-05-30 15:02:12 +0530

    useRef hook

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {Link} from 'react-router-dom'
 
 
 export default function Home() {
-    return <h1>Home. <Link to="/about">Go to About</Link></h1> 
+    
+    const h1Ref = useRef()
+
+    setTimeout(() => {
+        console.log(h1Ref.current.innerHTML)    
+    }, 10);
+    return <h1 ref={h1Ref}>Home <Link to="/about">Go to About</Link></h1> 
 }
   
